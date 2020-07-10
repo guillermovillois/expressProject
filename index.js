ab6ae40d41c02144ca4304152b8cfa4e7b5101fe
Guille <guillearg@gmail.com>
2020-07-10 12:35:27 -0300

    middleware

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 const express = require('express');
 const path = require('path');
+const members = require('./members');
 
 const app = express();
+
+const logger = (req, res, next) => {
+    console.log('Hello');
+    next();
+}
+
+//init middleware
+app.use(logger);
+// get members
+app.get('/api/members', (req, res) => res.json(members))
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'public', 'index.html'));
+//     // res.send('<h1>Hello World Guille!</h1>')
+// })
+
+// static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 const PORT = process.env.PORT || 3000;
 
---
+++ members.js
@@ -0,0 +1,12 @@
+const members = [
+    {
+        id: 1,
+        name: 'guille'
+    },
+    {
+        id: 2,
+        name: 'andres'
+    }
+]
+
+module.exports = members;
